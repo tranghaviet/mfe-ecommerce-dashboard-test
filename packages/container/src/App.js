7074390285093ab658e2dbb7e673f796e694c57b
Trang Ha Viet <viettrangha@gmail.com>
2024-05-30 22:52:29 +0700

    add container root React.js app

---
+++ packages/container/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function () {
+  return <h1>Hi from container</h1>
+};
---
+++ packages/container/src/bootstrap.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+
+// import Marketing
+
+// mount function to start up the app
+ReactDom.render(
+  <App/>,
+  document.getElementById('root')
+)
