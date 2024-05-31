4d1495f4228e1579a53aeab924c9b516f0fe6bba
Trang Ha Viet <viettrangha@gmail.com>
2024-05-31 15:07:34 +0700

    production webpack config for container

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const commonConfig = require('./webpack.common')
@@ -23,11 +22,7 @@ const devConfig = {
       },
       shared: packageJson.dependencies
     }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
   ]
 }
-
 
 module.exports = merge(commonConfig, devConfig)
