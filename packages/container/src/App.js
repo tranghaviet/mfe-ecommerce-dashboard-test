e5f1cba0c77b887e967b653908149131a8979308
Trang Ha Viet <viettrangha@gmail.com>
2024-06-01 23:59:32 +0700

    automate cloudfront invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp.js'
 
 export default function () {
   return <div>
-    <h1>Hi from container</h1>
+    <h1>Hi</h1>
     <hr/>
     <MarketingApp/>
   </div>
