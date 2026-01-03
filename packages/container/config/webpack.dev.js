c8ba61dacd1dd59c66893ed5a288cb3d19779e00
Trang Ha Viet <viettrangha@gmail.com>
2026-01-03 20:11:00 +0700

    feat: add missing webpack-cli, upgraded SASS

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -22,9 +22,9 @@ const devConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        // marketing: 'marketing@http://localhost:8081/remoteEntry.js',
         auth: 'auth@http://localhost:8082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+        // dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
       },
       shared: packageJson.dependencies
     }),
